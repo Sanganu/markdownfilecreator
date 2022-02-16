b29d32e6b3314232942005f581b521f25ef7f333
Sanganu <kppkmsanu@gmail.com>
2022-02-16 16:42:13 -0700

    Error in Async function

--- utils/getGithubProfile.js
+++ utils/getGithubProfile.js
@@ -1,14 +1,18 @@
-module.exports = ({login,following,followers,email,public_repo}) =>{
-    return(`# ABOUT THE AUTHOR
+module.exports = ({login,following,followers,email,public_repos}) =>{
+    return(`
+        <br>
+        (____)
+        # ABOUT THE AUTHOR
     
         ## GITHUB USERNAME: ${login}
             
-        Email: ${email}
+        Email on Github Profile: ${email}
             
         Following: ${following}
             
         Followers: ${followers}
             
-        Public repo: ${[public_repos]}`
+        Public repo: ${public_repos}`
+        (____)
         )
 }
